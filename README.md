3dfa617660f0610f863f280e491223c7f3afcb36
izbay <jacobmartinek@yahoo.com>
2014-01-14 01:12:41 -0500

    Project Initialization
    The plugin is a blank slate at the moment. This commit is present to aid
    the other developers with environment setup.

---
+++ README.md
@@ -0,0 +1,1 @@
+SiegeEngine Readme.
---
+++ src/com/github/izbay/siegeengine/SiegeEnginePlugin.java
@@ -0,0 +1,16 @@
+package com.github.izbay.siegeengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SiegeEnginePlugin extends JavaPlugin {
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+}
